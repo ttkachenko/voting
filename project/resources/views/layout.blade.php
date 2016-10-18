7f60620b39b58c8698a838dbf39e6a0588af8f28
Tatiana Tkachenko <ttkachenko@oggettoweb.com>
2016-10-18 16:58:41 +0300

    Change title
    [re:31122]

--- project/resources/views/layout.blade.php
+++ project/resources/views/layout.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Laravel</title>
+    <title>Лучшие Люди</title>
     <meta name="_token" content="<?php echo csrf_token(); ?>">
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
@@ -21,7 +21,6 @@
     <div class="content">
 
         @yield('content')
-
 
     </div>
 </div>
